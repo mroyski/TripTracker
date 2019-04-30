bea335736b40f7ef949a5d6b3ad39c8f87122953
mroyski <mroyski@gmail.com>
2019-04-30 09:03:55 -0400

    swashbuckle installed and setup

--- TripTracker.BackService/Startup.cs
+++ TripTracker.BackService/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TripTracker.BackService
 {
@@ -27,11 +28,20 @@ namespace TripTracker.BackService
         {
             services.AddTransient<Models.Repository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(options =>
+            options.SwaggerDoc("v1", new Info { Title = "TripTracker", Version = "v1" })
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
